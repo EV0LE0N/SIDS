33e96a0be22751d7dc97575dee961c7400807a82
vea <lihaojun754@gmail.com>
2026-03-15 16:08:11 +0800

    修复了 pyspark 的路径引用问题

--- run_etl.sh
+++ run_etl.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 echo "🚀 开始执行 ETL 数据处理任务..."
-docker compose exec spark spark-submit \
+docker compose exec spark /opt/spark/bin/spark-submit \
     --master local[*] \
     --conf spark.driver.extraJavaOptions="-Djava.io.tmpdir=/tmp" \
     /app/spark_jobs/etl_clean.py
